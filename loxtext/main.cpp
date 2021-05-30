e3307acd9f90d70cb3b86f0f79a398e6f9549bbe
Morgan <hx801@nyu.edu>
2021-05-31 02:31:38 +0800

    Initial Commit

---
+++ loxtext/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  loxtext
+//
+//  Created by Morgan Xu on 5/31/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
